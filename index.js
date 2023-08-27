00e8530dc0e7e7ef22cd0ba628e53ba5bc740a67
Sudharaka <sudharakafernandobusiness@gmail.com>
2023-08-28 00:39:20 +0530

    Added the models and routes

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
+const customerRoute = require("./routes/customer");
+const customerInqueryRoute = require("./routes/customerInquery");
+const customerRequestRoute = require("./routes/customerRequest");
+const notificationRoute = require("./routes/notificationRoute");
 const cors = require("cors");
 
 
@@ -27,6 +31,10 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
+app.use("/api/customers", customerRoute);
+app.use("/api/inqueries", customerInqueryRoute);
+app.use("/api/requests", customerRequestRoute);
+app.use("/api/notifications",  notificationRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
