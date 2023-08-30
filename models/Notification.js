33f735ac32a2b25ae04d8535396af662a56f821d
Pasindu Madhuwantha <apmaduwantha@gmail.com>
2023-08-30 16:17:58 +0530

    remove unnecessary  models, user fields required true

--- models/Notification.js
+++ models/Notification.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const NotificationSchema = new mongoose.Schema(
   {
-    userID: { type: String, required: true},
+    userID: { type: String},
     userName: { type: String, required: true},
-    senderAddress: { type: String, required: true },
+    senderAddress: { type: String },
     isReply: {type:Boolean, default: false},
     hasLetter: {type: Boolean, default: false},
-    message: {type: String, required: false},
+    message: {type: String},
   },
   { timestamps: true }
 );
