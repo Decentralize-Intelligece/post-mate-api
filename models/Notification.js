4aad1e4643333d1ca9da5597cc21b1bd71138107
Thimira Nirmal <timnirmal@gmail.com>
2023-08-30 21:20:03 +0530

    Fixing Notification

--- models/Notification.js
+++ models/Notification.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 
 const NotificationSchema = new mongoose.Schema(
   {
-    userID: { type: String},
     userName: { type: String, required: true},
     senderAddress: { type: String },
     isReply: {type:Boolean, default: false},
     hasLetter: {type: Boolean, default: false},
-    message: {type: String},
+    message: {type: String}
   },
   { timestamps: true }
 );
