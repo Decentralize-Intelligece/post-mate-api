1fbc89c861c9aef72325eb34f5cb94cda4904005
Pasindu Madhuwantha <apmaduwantha@gmail.com>
2023-08-30 17:20:22 +0530

    postal cost

---
+++ models/PostalCost.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");   
+
+const PostalCostSchema = new mongoose.Schema(
+    {
+        weight: { type: String },
+        cost: { type: String },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("PostalCost", PostalCostSchema);
---
+++ routes/postalCost.js
@@ -0,0 +1,50 @@
+const PostalCost = require("../models/PostalCost");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+
+// router.post("/", verifyToken, async (req, res) => {
+router.post("/",  async (req, res) => {
+    console.log(req.body);
+
+  const cost = new PostalCost(req.body);
+
+  try {
+    const savedCost = await cost.save();
+    res.status(200).json(savedCost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET COST BY WEIGHT
+router.get("/find/:weight", async (req, res) => {
+    try {
+        const cost = await PostalCost.find({weight: req.params.weight });
+        res.status(200).json(cost);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+});
+
+
+
+//GET ALL
+router.get("/",  async (req, res) => {
+    try {
+        const cost = await PostalCost.find();
+        res.status(200).json(cost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
